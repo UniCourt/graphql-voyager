9f2b078455ff237915528b0e515c5b3c17b93f06
Jaison-uc <jaisonc@unicourt.com>
2021-02-08 16:45:39 +0530

    API changes for local kube testing

--- demo/presets.ts
+++ demo/presets.ts
@@ -1,16 +1,2 @@
-const githubIntrospection = require('./presets/github_introspection.json');
-const swapiIntrospection = require('./presets/swapi_introspection.json');
-const yelpIntrospection = require('./presets/yelp_introspection.json');
-const shopifyIntrospection = require('./presets/shopify_introspection.json');
 const api_v2 = require('./presets/api_v2.json')
-
-export const PRESETS = {
-  'Star Wars': swapiIntrospection,
-  Yelp: yelpIntrospection,
-  'Shopify Storefront': shopifyIntrospection,
-  GitHub: githubIntrospection,
-  'API V2': api_v2
-};
-
-export const defaultPresetName = 'API V2';
-export const defaultPreset = PRESETS[defaultPresetName];
+export const defaultPreset = api_v2;
