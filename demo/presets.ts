cd5ac1d176e753ec0c1704b62697831bf56f849c
Jaison-uc <jaisonc@unicourt.com>
2021-02-09 10:00:42 +0530

    API changes for local kube testing

--- demo/presets.ts
+++ demo/presets.ts
@@ -1,2 +1,6 @@
-const api_v2 = require('./presets/api_v2.json')
-export const defaultPreset = api_v2;
+const api_v2 = require('./presets/api_v2.json');
+export const PRESETS = {
+  'API V2': api_v2
+};
+export const defaultPresetName = 'API V2';
+export const defaultPreset = PRESETS[defaultPresetName];
